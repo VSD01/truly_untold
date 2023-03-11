5312dd9d5f76f9cffe25bc9b75ba95d2a3787740
jhashubham123 <91955988+jhashubham123@users.noreply.github.com>
2023-03-11 20:22:04 +0530

    all done

--- lib/app/pages/home/home_view.dart
+++ lib/app/pages/home/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:practise1/app/app.dart';
 import 'package:practise1/app/pages/home/home.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,69 +9,117 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Responsive.isMobile(context) ? mobileAppBarWidget() : Container(),
       appBar: AppBar(
+        automaticallyImplyLeading: Responsive.isMobile(context) ? true : false,
         backgroundColor: Colors.transparent,
         elevation: 0,
         centerTitle: true,
-        title: Row(
-          children: [
-            Spacer(),
-            Row(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    Get.dialog(
-                      Container(
-                        color: Colors.white.withOpacity(0.5),
-                      ),
-                    );
-                  },
-                  child: Text(
-                    'About',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                SizedBox(
-                  width: 100,
-                ),
-                Text(
-                  'Gallery',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  width: 100,
-                ),
-                Text(
-                  'Blog',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  width: 100,
-                ),
-                Text(
-                  'Contact',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            Spacer(),
-          ],
-        ),
+        title: Responsive.isWeb(context) ? webAppBar() : Container(),
       ),
       backgroundColor: Colors.black,
       body: HomeWidget(),
     );
   }
+}
+
+Widget webAppBar() {
+  return Row(
+    children: [
+      Spacer(),
+      Row(
+        children: [
+          GestureDetector(
+            onTap: () {
+              Get.dialog(
+                Container(
+                  color: Colors.white.withOpacity(0.5),
+                ),
+              );
+            },
+            child: Text(
+              'About',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            width: 100,
+          ),
+          Text(
+            'Gallery',
+            style: TextStyle(
+                color: Colors.white, fontSize: 17, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            width: 100,
+          ),
+          Text(
+            'Blog',
+            style: TextStyle(
+                color: Colors.white, fontSize: 17, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            width: 100,
+          ),
+          Text(
+            'Contact',
+            style: TextStyle(
+                color: Colors.white, fontSize: 17, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+      Spacer(),
+    ],
+  );
+}
+
+Widget mobileAppBarWidget() {
+  return Drawer(
+    backgroundColor: Colors.white.withOpacity(0.7),
+    child: Column(
+      children: [
+        GestureDetector(
+          onTap: () {
+            Get.dialog(
+              Container(
+                color: Colors.black.withOpacity(0.5),
+              ),
+            );
+          },
+          child: Text(
+            'About',
+            style: TextStyle(
+                color: Colors.white, fontSize: 17, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(
+          width: 100,
+        ),
+        Text(
+          'Gallery',
+          style: TextStyle(
+              color: Colors.white, fontSize: 17, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          width: 100,
+        ),
+        Text(
+          'Blog',
+          style: TextStyle(
+              color: Colors.white, fontSize: 17, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          width: 100,
+        ),
+        Text(
+          'Contact',
+          style: TextStyle(
+              color: Colors.white, fontSize: 17, fontWeight: FontWeight.bold),
+        ),
+      ],
+    ),
+  );
 }
--- lib/app/pages/home/widget/home_widget.dart
+++ lib/app/pages/home/widget/home_widget.dart
@@ -19,80 +19,15 @@ class HomeWidget extends StatelessWidget {
                 horizontal: Responsive.isMobile(context) ? 30 : 200),
             child: Column(
               children: [
-                Expanded(
-                  child: FadeTransition(
-                    opacity: controller.animation!,
-                    child: Opacity(
-                      opacity: 0.5,
-                      child: Column(
-                        children: [
-                          Text(
-                            'TRULY',
-                            style: GoogleFonts.oswald(
-                                color: Colors.white,
-                                fontSize: Dimens.fifteen,
-                                fontWeight: FontWeight.bold,
-                                letterSpacing: 50),
-                          ),
-                          Text(
-                            'UNTOLD',
-                            style: GoogleFonts.oswald(
-                                color: Colors.white,
-                                fontSize: Dimens.fifteen,
-                                fontWeight: FontWeight.bold,
-                                letterSpacing: 50),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 2,
-                  child: Container(
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              alignment: Alignment.center,
-                              image: NetworkImage(
-                                  "https://th.bing.com/th/id/R.c81ae7b371e44d77dfc7f5e822047ced?rik=QFGQxInWl6gosg&riu=http%3a%2f%2fcdn.mysitoo.com%2f10750%2fcache%2fatn1024_Poster+Two+Face+-+Tom+Levin+web.jpg%3fv%3d1414619492&ehk=s%2foAn%2b4SyMyk%2bCF0Sr6S7EVJv6mSMg10sgLXQdHGhvE%3d&risl=&pid=ImgRaw&r=0")))),
-                ),
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.centerRight,
-                    child: Container(
-                      width: Dimens.percentWidth(0.5),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            width: Dimens.getWidthPercent(0.3),
-                            child: AnimatedTextKit(
-                              isRepeatingAnimation: false,
-                              animatedTexts: [
-                                TypewriterAnimatedText(
-                                    'सत्य को किसी प्रमाण की आवश्यकता नहीं होती, लेकिन अक्सर लोग इस पर विश्वास करने से डरते हैं और विश्वास करने से इंकार कर देते हैं।'
-                                    'लेकिन कभी-कभी सच को साबित करना होता है, इसलिए चाहे जो भी हो, हम ट्रूली अनटोल्ड में आपका स्वागत करते हैं और दुनिया\n\n में हो रही किसी भी घटना और ऐसी ही कुछ कहानियों का सच सामने लाने की कोशिश करते हैं। हम उन कहानियों पर भी चर्चा करेंगे जो आपको दंग कर देंगी।'
-                                    'अगर आप हमारी दुनिया के बारे में सुनना पसंद करते हैं तो हमारे चैनल को अभी सब्सक्राइब करें',
-                                    textStyle: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: Dimens.three,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                    speed: Duration(milliseconds: 60),
-                                    textAlign: TextAlign.center),
-                              ],
-                            ),
-                          ),
-                          Container(
-                            height: 135,
-                            width: 135,
-                            child: Image.asset('assets/logo.png'),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+                ///Logo text here------
+                mainTitleWidget(),
+
+                ////Center image widget here------
+                centerImageWidget(),
+
+                ///Bio widget here---------
+                bioWidget(context),
+                Dimens.boxHeight10,
               ],
             ),
           ),
@@ -100,4 +35,75 @@ class HomeWidget extends StatelessWidget {
       );
     });
   }
+}
+
+///Logo text here
+Widget mainTitleWidget() {
+  return GetBuilder<HomeController>(builder: (controller) {
+    return FittedBox(
+      child: FadeTransition(
+        opacity: controller.animation!,
+        child: Opacity(
+          opacity: 0.5,
+          child: Column(
+            children: [
+              Text(
+                'TRULY',
+                style: GoogleFonts.oswald(
+                    color: Colors.white,
+                    fontSize: 80,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 50),
+              ),
+              Text(
+                'UNTOLD',
+                style: GoogleFonts.oswald(
+                    color: Colors.white,
+                    fontSize: 80,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 50),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  });
+}
+
+///Center image widget here
+Widget centerImageWidget() {
+  return Expanded(
+    flex: 2,
+    child: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(
+                    "https://th.bing.com/th/id/R.c81ae7b371e44d77dfc7f5e822047ced?rik=QFGQxInWl6gosg&riu=http%3a%2f%2fcdn.mysitoo.com%2f10750%2fcache%2fatn1024_Poster+Two+Face+-+Tom+Levin+web.jpg%3fv%3d1414619492&ehk=s%2foAn%2b4SyMyk%2bCF0Sr6S7EVJv6mSMg10sgLXQdHGhvE%3d&risl=&pid=ImgRaw&r=0")))),
+  );
+}
+
+////Bio widget for web
+Widget bioWidget(BuildContext context) {
+  return Expanded(
+    child: Container(
+      width: Dimens.percentHeight(.50),
+      child: AnimatedTextKit(
+        isRepeatingAnimation: false,
+        animatedTexts: [
+          TypewriterAnimatedText(
+              'सत्य को किसी प्रमाण की आवश्यकता नहीं होती, लेकिन अक्सर लोग इस पर विश्वास करने से डरते हैं और विश्वास करने से इंकार कर देते हैं।'
+              'लेकिन कभी-कभी सच को साबित करना होता है, इसलिए चाहे जो भी हो, हम ट्रूली अनटोल्ड में आपका स्वागत करते हैं और दुनिया\n\n में हो रही किसी भी घटना और ऐसी ही कुछ कहानियों का सच सामने लाने की कोशिश करते हैं। हम उन कहानियों पर भी चर्चा करेंगे जो आपको दंग कर देंगी।'
+              'अगर आप हमारी दुनिया के बारे में सुनना पसंद करते हैं तो हमारे चैनल को अभी सब्सक्राइब करें',
+              textStyle: TextStyle(
+                color: Colors.white,
+                fontSize: Responsive.isMobile(context) ? 12 : 17,
+                fontWeight: FontWeight.w500,
+              ),
+              speed: Duration(milliseconds: 60),
+              textAlign: TextAlign.center),
+        ],
+      ),
+    ),
+  );
 }
