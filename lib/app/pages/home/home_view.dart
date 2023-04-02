47fe598cbd62c61272ec6f4dd8049629f03b5af4
jhashubham123 <jhaaji143@gmail.com>
2023-04-02 18:54:15 +0530

    all done

--- lib/app/pages/home/home_view.dart
+++ lib/app/pages/home/home_view.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:practise1/app/app.dart';
 
-
-
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -30,21 +28,18 @@ class HomeScreen extends StatelessWidget {
 }
 
 Widget webAppBar() {
-  return GetBuilder<HomeController>(
-    builder: (controller) {
-   
-  
-      // print('ip adress-view part------------------------${controller.data} ---  ${controller.data=='{ip: 122.173.29.168}'}');
-      return Row(
-        children: [
-          Spacer(),
-          Row(
-            children: [
-              GestureDetector(
-                onTap: () {
-                  Get.dialog(AboutScreen());
-                  
-                },
+  return GetBuilder<HomeController>(builder: (controller) {
+    // print('ip adress-view part------------------------${controller.data} ---  ${controller.data=='{ip: 122.173.29.168}'}');
+    return Row(
+      children: [
+        Spacer(),
+        Row(
+          children: [
+            GestureDetector(onTap: () {
+              Get.dialog(AboutScreen());
+            }, child: OnHover(builder: ((isHovered) {
+              return Tooltip(
+                message: 'About creators',
                 child: Text(
                   'About',
                   style: TextStyle(
@@ -52,14 +47,17 @@ Widget webAppBar() {
                       fontSize: 17,
                       fontWeight: FontWeight.bold),
                 ),
-              ),
-              SizedBox(
-                width: 100,
-              ),
-              GestureDetector(
-                onTap: () {
-                  NavigateTo.galleryScreen();
-                },
+              );
+            }))),
+            SizedBox(
+              width: 100,
+            ),
+            GestureDetector(
+              onTap: () {
+                NavigateTo.galleryScreen();
+              },
+              child: Tooltip(
+                message: 'Gallery',
                 child: Text(
                   'Gallery',
                   style: TextStyle(
@@ -68,26 +66,34 @@ Widget webAppBar() {
                       fontWeight: FontWeight.bold),
                 ),
               ),
-              SizedBox(
-                width: 100,
-              ),
-              GestureDetector(
-                onTap: (){
-                  NavigateTo.blogsScreen();
-                },
+            ),
+            SizedBox(
+              width: 100,
+            ),
+            GestureDetector(
+              onTap: () {
+                NavigateTo.blogsScreen();
+              },
+              child: Tooltip(
+                message: 'Blog',
                 child: Text(
                   'Blog',
                   style: TextStyle(
-                      color: Colors.white, fontSize: 17, fontWeight: FontWeight.bold),
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold),
                 ),
               ),
-              SizedBox(
-                width: 100,
-              ),
-              GestureDetector(
-                onTap: () {
-                  Get.dialog(ContactScreen());
-                },
+            ),
+            SizedBox(
+              width: 100,
+            ),
+            GestureDetector(
+              onTap: () {
+                Get.dialog(ContactScreen());
+              },
+              child: Tooltip(
+                message: 'Contact',
                 child: Text(
                   'Contact',
                   style: TextStyle(
@@ -95,199 +101,63 @@ Widget webAppBar() {
                       fontSize: 17,
                       fontWeight: FontWeight.bold),
                 ),
-             
-
               ),
-              controller.isIpmatched==true?
-               SizedBox(
-                width: 100,
-              ):Container(),
-               controller.isIpmatched==true?
-             
-              GestureDetector(
-                onTap: () {
-                Get.dialog(Scaffold(
-                  body: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                     
-                                  children: [
-                    GestureDetector(
-                      onTap: (){
-                         NavigateTo.menuPageScreen();
-                      },
-                      child: Container(
-                        alignment: Alignment.center,
-                        width: Get.width*0.70,
-                        height: Get.height*0.05,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          border: Border.all(color: Colors.grey)
-                        ),
-                        child: Text('Add Blog',style: AppTextStyle.black_17_400,),
-                      ),
-                    ),
-                    SizedBox(height: Get.height*0.05,),
-                    GestureDetector(
-                      onTap: (){
-                        NavigateTo.addImageScreen();
-                      },
-                      child: Container(
-                        alignment: Alignment.center,
-                        width: Get.width*0.70,
-                        height: Get.height*0.05,
-                        decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.circular(15),
-                         
-                        ),
-                        child: Text('Add Image',style: AppTextStyle.white_17_400,),
-                      ),
-                    )
-                                  ],
-                    ),
-                  ),
-                ));
-                },
-                child: Text(
-                  'Admin',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold),
-                ),
-             
-
-              ):Container(),
-
-            ],
-          ),
-          Spacer(),
-        ],
-      );
-    }
-  );
-}
-
-Widget mobileAppBarWidget() {
-  return GetBuilder<HomeController>(
-    builder: (controller) {
-      return Container(
-        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
-        decoration: BoxDecoration(
-            color: Colors.white.withOpacity(0.9),
-            borderRadius: BorderRadius.only(
-                topRight: Radius.circular(20), bottomRight: Radius.circular(20))),
-        child: Drawer(
-          width: Dimens.percentWidth(.40),
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Dimens.boxHeight20,
-              Container(
-                  decoration: BoxDecoration(
-                      color: Colors.black, borderRadius: BorderRadius.circular(20)),
-                  child: Image.asset('assets/logo.png')),
-              Dimens.boxHeight20,
-              GestureDetector(
-                onTap: () {
-                  Get.back();
-                  Get.dialog(AboutScreen());
-                },
-                child: Text(
-                  'About',
-                  style: AppTextStyle.black_17_700,
-                ),
-              ),
-              Dimens.boxHeight15,
-              GestureDetector(
-                onTap: () {
-                  Get.back();
-                  Get.dialog(GalleryScreen());
-                },
-                child: GestureDetector(
-                  onTap: (){
-                    NavigateTo.galleryScreen();
-                  },
-                  child: Text(
-                    'Gallery',
-                    style: AppTextStyle.black_17_700,
-                  ),
-                ),
-              ),
-              Dimens.boxHeight15,
-              GestureDetector(
-                onTap: (){
-                   NavigateTo.menuPageScreen();
-                },
-                child: Text(
-                  'Blog',
-                  style: AppTextStyle.black_17_700,
-                ),
-              ),
-              Dimens.boxHeight15,
-              GestureDetector(
-                onTap: () {
-                  Get.back();
-                  Get.dialog(ContactScreen());
-                },
-                child: Text(
-                  'Contact',
-                  style: AppTextStyle.black_17_700,
-                ),
-              ),
-               controller.isIpmatched==true?
-                   SizedBox(
+            ),
+            controller.isIpmatched == true
+                ? SizedBox(
                     width: 100,
-                  ):Container(),
-                   controller.isIpmatched==true?
-                 
-                  GestureDetector(
+                  )
+                : Container(),
+            controller.isIpmatched == true
+                ? GestureDetector(
                     onTap: () {
-                    Get.dialog(Scaffold(
-                      body: Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                         
-                                      children: [
-                        GestureDetector(
-                          onTap: (){
-                             NavigateTo.menuPageScreen();
-                          },
-                          child: Container(
-                            alignment: Alignment.center,
-                            width: Get.width*0.70,
-                            height: Get.height*0.05,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(15),
-                              border: Border.all(color: Colors.grey)
-                            ),
-                            child: Text('Add Blog',style: AppTextStyle.black_17_400,),
+                      Get.dialog(Scaffold(
+                        body: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  NavigateTo.menuPageScreen();
+                                },
+                                child: Container(
+                                  alignment: Alignment.center,
+                                  width: Get.width * 0.70,
+                                  height: Get.height * 0.05,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(15),
+                                      border: Border.all(color: Colors.grey)),
+                                  child: Text(
+                                    'Add Blog',
+                                    style: AppTextStyle.black_17_400,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: Get.height * 0.05,
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  NavigateTo.addImageScreen();
+                                },
+                                child: Container(
+                                  alignment: Alignment.center,
+                                  width: Get.width * 0.70,
+                                  height: Get.height * 0.05,
+                                  decoration: BoxDecoration(
+                                    color: Colors.black,
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                  child: Text(
+                                    'Add Image',
+                                    style: AppTextStyle.white_17_400,
+                                  ),
+                                ),
+                              )
+                            ],
                           ),
                         ),
-                        SizedBox(height: Get.height*0.05,),
-                        GestureDetector(
-                          onTap: (){
-                            NavigateTo.addImageScreen();
-                          },
-                          child: Container(
-                            alignment: Alignment.center,
-                            width: Get.width*0.70,
-                            height: Get.height*0.05,
-                            decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.circular(15),
-                             
-                            ),
-                            child: Text('Add Image',style: AppTextStyle.white_17_400,),
-                          ),
-                        )
-                                      ],
-                        ),
-                      ),
-                    ));
+                      ));
                     },
                     child: Text(
                       'Admin',
@@ -296,13 +166,153 @@ Widget mobileAppBarWidget() {
                           fontSize: 17,
                           fontWeight: FontWeight.bold),
                     ),
-                 
-
-                  ):Container(),
-            ],
-          ),
+                  )
+                : Container(),
+          ],
         ),
-      );
-    }
-  );
+        Spacer(),
+      ],
+    );
+  });
+}
+
+Widget mobileAppBarWidget() {
+  return GetBuilder<HomeController>(builder: (controller) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+      decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.9),
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(20), bottomRight: Radius.circular(20))),
+      child: Drawer(
+        width: Dimens.percentWidth(.40),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Dimens.boxHeight20,
+            Container(
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Image.asset('assets/logo.png')),
+            Dimens.boxHeight20,
+            GestureDetector(
+              onTap: () {
+                Get.back();
+                Get.dialog(AboutScreen());
+              },
+              child: Text(
+                'About',
+                style: AppTextStyle.black_17_700,
+              ),
+            ),
+            Dimens.boxHeight15,
+            GestureDetector(
+              onTap: () {
+                Get.back();
+                Get.dialog(GalleryScreen());
+              },
+              child: GestureDetector(
+                onTap: () {
+                  NavigateTo.galleryScreen();
+                },
+                child: Text(
+                  'Gallery',
+                  style: AppTextStyle.black_17_700,
+                ),
+              ),
+            ),
+            Dimens.boxHeight15,
+            GestureDetector(
+              onTap: () {
+                NavigateTo.menuPageScreen();
+              },
+              child: Text(
+                'Blog',
+                style: AppTextStyle.black_17_700,
+              ),
+            ),
+            Dimens.boxHeight15,
+            GestureDetector(
+              onTap: () {
+                Get.back();
+                Get.dialog(ContactScreen());
+              },
+              child: Text(
+                'Contact',
+                style: AppTextStyle.black_17_700,
+              ),
+            ),
+            controller.isIpmatched == true
+                ? SizedBox(
+                    width: 100,
+                  )
+                : Container(),
+            controller.isIpmatched == true
+                ? GestureDetector(
+                    onTap: () {
+                      Get.dialog(Scaffold(
+                        body: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  NavigateTo.menuPageScreen();
+                                },
+                                child: Container(
+                                  alignment: Alignment.center,
+                                  width: Get.width * 0.70,
+                                  height: Get.height * 0.05,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(15),
+                                      border: Border.all(color: Colors.grey)),
+                                  child: Text(
+                                    'Add Blog',
+                                    style: AppTextStyle.black_17_400,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: Get.height * 0.05,
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  NavigateTo.addImageScreen();
+                                },
+                                child: Container(
+                                  alignment: Alignment.center,
+                                  width: Get.width * 0.70,
+                                  height: Get.height * 0.05,
+                                  decoration: BoxDecoration(
+                                    color: Colors.black,
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                  child: Text(
+                                    'Add Image',
+                                    style: AppTextStyle.white_17_400,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ));
+                    },
+                    child: Text(
+                      'Admin',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  )
+                : Container(),
+          ],
+        ),
+      ),
+    );
+  });
 }
